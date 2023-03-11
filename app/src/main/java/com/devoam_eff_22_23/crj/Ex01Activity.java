25c94c4e91553f4d1cbd8f2494d35b6f169a1efb
Anouar DERDOURI <anouar.derdouri.development@gmail.com>
2023-03-11 08:51:29 +0000

    Initial commit

---
+++ app/src/main/java/com/devoam_eff_22_23/crj/Ex01Activity.java
@@ -0,0 +1,14 @@
+package com.devoam_eff_22_23.crj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex01Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ex01);
+    }
+}
